5b516a7e7af31c6f0d92133dee7742dd563dc5ad
Sanoj404 <56234552+Sanoj404@users.noreply.github.com>
2021-05-09 23:52:08 +0530

    ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,30 +2,13 @@ resource "aws_instance" "engine1" {
     ami = var.ami
     instance_type = "t2.micro"
     # VPC
-    subnet_id = aws_subnet.private_subnet.id
+    #subnet_id = aws_subnet.private_subnet.id
     # Security Group
-    vpc_security_group_ids = [aws_security_group.ssh-allowed.id]
+    #vpc_security_group_ids = [aws_security_group.ssh-allowed.id]
     
     # the Public SSH key
     key_name = var.key_name
     tags = {
     Name = "engine1"
-  }
-}
-resource "aws_eip" "lb1" {
-  instance = aws_instance.engine1.id
-  vpc      = true
-}
-resource "aws_instance" "engine2" {
-    ami = var.ami
-    instance_type = "t2.micro"
-    # VPC
-    subnet_id = aws_subnet.public_subnet.id
-    # Security Group
-    vpc_security_group_ids = [aws_security_group.ssh-notallowed.id]
-    # the Public SSH key
-    key_name = var.key_name
-    tags = {
-    Name = "engine2"
   }
 }
