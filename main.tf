6245e47a4f18ea39f3e71edabc21f17ea08c6563
Sanoj404 <56234552+Sanoj404@users.noreply.github.com>
2021-05-09 22:53:54 +0530

    updated tffiles1

--- main.tf
+++ main.tf
@@ -11,3 +11,14 @@ terraform {
     #dynamodb_table = "remote-lock-tc" name of the dynamodb which created.
   }
 }
+resource "aws_instance" "web1" {
+    ami = "ami-0d5eff06f840b45e9"
+    instance_type = "t2.micro"
+    key_name = "bpxeukey.pem"
+    subnet_id = "subnet-04916f0beb56c06c5"
+    vpc_security_group_ids = [ "sg-0c5fe5a21f7d4ad49" ]
+    security_groups = [ "sg-0c5fe5a21f7d4ad49" ]
+    tags = {
+      "Name" = "Node3"
+    }
+}
