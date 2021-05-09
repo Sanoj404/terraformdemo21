4b05455db45ff20b6a2d00f9791b631851b33a0a
Sanoj404 <56234552+Sanoj404@users.noreply.github.com>
2021-05-09 22:27:04 +0530

    update tf file

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+
+  backend "s3" {
+    bucket     = "myqabucketforterraformtest"              #bucket name
+    key        = "terraform-statefile" #giving file directory or folder name where to store state file.
+    region     = "us-east-1"
+    #dynamodb_table = "remote-lock-tc" name of the dynamodb which created.
+  }
+}
