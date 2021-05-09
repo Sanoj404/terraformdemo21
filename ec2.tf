ffda3749c5d604d644241a93b4fbd722f99702bb
Sanoj404 <56234552+Sanoj404@users.noreply.github.com>
2021-05-09 23:57:09 +0530

    updated EC2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "engine1" {
     ami = var.ami
     instance_type = "t2.micro"
     # VPC
-    #subnet_id = aws_subnet.private_subnet.id
+    # subnet_id = aws_subnet.private_subnet.id
     # Security Group
     #vpc_security_group_ids = [aws_security_group.ssh-allowed.id]
     
