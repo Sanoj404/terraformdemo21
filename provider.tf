6950c290af1f3d78e0738d10a6556a5e633e0599
Sanoj404 <56234552+Sanoj404@users.noreply.github.com>
2021-05-10 00:01:53 +0530

    provider file updated

--- provider.tf
+++ provider.tf
@@ -7,7 +7,8 @@ terraform {
     bucket     = "myqabucketforterraformtest"              #bucket name
     key        = "terraform-statefile" #giving file directory or folder name where to store state file.
     region     = "us-east-1"
+    access_key = "AKIA2LRYUQNPGCF5I67Z"
+    secret_key = "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"
     #dynamodb_table = "remote-lock-tc" name of the dynamodb which created.
   }
 }
-
