7140ff44045e60fbc7059643dfd60f01d0c1f48f
Sanoj404 <56234552+Sanoj404@users.noreply.github.com>
2021-05-09 23:47:07 +0530

    updating tf files

--- provider.tf
+++ provider.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region = var.region
 }
-
 terraform {
 
   backend "s3" {
@@ -11,14 +10,4 @@ terraform {
     #dynamodb_table = "remote-lock-tc" name of the dynamodb which created.
   }
 }
-resource "aws_instance" "web1" {
-    ami = "ami-0d5eff06f840b45e9"
-    instance_type = "t2.micro"
-    key_name = "bpxeukey.pem"
-    subnet_id = "subnet-04916f0beb56c06c5"
-    vpc_security_group_ids = [ "sg-0c5fe5a21f7d4ad49" ]
-    security_groups = [ "sg-0c5fe5a21f7d4ad49" ]
-    tags = {
-      "Name" = "Node3"
-    }
-}
+
