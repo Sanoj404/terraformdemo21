036bb8c16d5428ea4ba98d31ff87c0ea0d5d2d91
Sanoj404 <56234552+Sanoj404@users.noreply.github.com>
2021-05-10 00:20:32 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 terraform {
 
   backend "s3" {
-    bucket     = "myqabucketforterraformtest"              #bucket name
+    bucket     = "myqabucketforterraformtest"    #bucket name
     key        = "terraform-statefile" #giving file directory or folder name where to store state file.
     region     = "us-east-1"
     #dynamodb_table = "remote-lock-tc" name of the dynamodb which created.
